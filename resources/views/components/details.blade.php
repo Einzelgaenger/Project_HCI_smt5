c06c52f210cf6130fa4c0ab1dd51b57a08d77ca7
Alvin-Winst <alvinrw04@gmail.com>
2024-12-05 23:16:16 +0700

    Update: component details

--- resources/views/components/details.blade.php
+++ resources/views/components/details.blade.php
@@ -1,12 +1,3 @@
-<div class="bg-black mb-8 mt-4 ml-8 w-2/5 h-5/6 rounded p-5 lg:w-1/4">
-    <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="CellSignalLow.svg" class="w-6 sm:w-7">{{$difficulty}}</p>
-    <hr class="my-1">
-    <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="Clock.svg" class="w-6 sm:w-7">{{$duration}} hours</p>
-    <hr class="my-1">
-    <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="BookBookmark.svg" class="w-6 sm:w-7">{{$lessons}} lessons</p>
-    <hr class="my-1">
-    <div class="flex gap-4 mt-[15%] md:mt-[8%]">
-        <button class="{{$bookmarked ? 'bg-white text-black' : 'border-[#2c2c2c] border-2 text-white'}} rounded px-2 py-1 flex items-center gap-1 text-xs sm:text-sm md:text-md lg:text-lg"><img src="BookmarkSimple.svg" class="{{$bookmarked ? 'invert' : 'invert-0'}}"><p class="hidden md:block">Save</p></button>
-        <button class="{{$liked ? 'bg-white text-black' : 'border-[#2c2c2c] border-2 text-white'}} rounded p-2 flex items-center gap-1  text-xs sm:text-sm md:text-md lg:text-lg"><img src="ThumbsUp.svg" class="{{$liked ? 'invert' : 'invert-0'}}"><p class="hidden md:block">{{$likes}}</p></button>
-    </div>
+<div class="bg-black mb-8 mt-4 ml-8 w-2/5 rounded p-5 lg:w-1/4">
+    {{$slot}}
 </div>
--- resources/views/syllabus.blade.php
+++ resources/views/syllabus.blade.php
@@ -6,10 +6,6 @@
 <div class="py-6 px-10">
     <x-title-card>
         <article class="w-4/5">
-            {{-- <div class="flex items-center gap-x-4 text-xs">
-                <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-            </div> --}}
             <div class="my-7 ml-3 mr-6 w-full">
                 <h3 class="text-2xl w-full lg:w-3/4 font-black text-white sm:text-3xl overflow-hidden line-clamp-2">
                 Boost your conversion rate Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
@@ -17,14 +13,24 @@
                 <p class="mt-3 text-xs/5 w-full lg:w-11/12 text-white sm:text-sm/6 xl:text-base h-[6.5rem] sm:h-24 overflow-hidden line-clamp-5 sm:line-clamp-4">Brief description of the syllabus, what to the user expect to learn, what they will do, and so on. Make it a bit more longer to make sure  a long paragraph would fit perfectly to the design. Please don’t read these, it’s for eliminating white spaces. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem illo magni animi nisi accusamus aut quam expedita ut dolorem, itaque, corporis rerum dolores tempore quasi excepturi maiores minus. Nihil veritatis sed provident a, voluptatem, corrupti consequatur minus tempora dicta accusantium ullam cupiditate eligendi impedit reprehenderit asperiores laboriosam non magni ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, nobis id amet adipisci hic voluptates voluptate mollitia reiciendis fugiat, voluptas aspernatur eligendi quam. Non, modi nihil molestiae iure temporibus error perspiciatis id numquam? Corrupti ad veritatis nisi voluptates voluptatem harum, expedita, vitae quam, ipsam delectus architecto explicabo. Perferendis, eum. Quia!</p>
             </div>
         </article>
-        @include('components.details', [
-            'difficulty' => 'Beginner Friendly',
-            'duration' => 6,
-            'lessons' => 12,
-            'bookmarked' => false,
-            'liked' => true,
-            'likes' => 256
-        ])
+        <x-details>
+            <div class="h-5/6">
+                <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="CellSignalLow.svg" class="w-6 sm:w-7">Beginner Friendly</p>
+                <hr class="my-1">
+                <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="Clock.svg" class="w-6 sm:w-7">4 hours</p>
+                <hr class="my-1">
+                <p class="text-white text-xs sm:text-sm md:text-md lg:text-lg flex items-center gap-1 overflow-hidden"><img src="BookBookmark.svg" class="w-6 sm:w-7">12 lessons</p>
+                <hr class="my-1">
+                <div class="flex gap-4 mt-[15%] md:mt-[8%]">
+                    {{-- PAKE YG DI-COMMENT (BE) --}}
+                    {{-- <button class="{{$bookmarked ? 'bg-white text-black' : 'border-[#2c2c2c] border-2 text-white'}} rounded px-2 py-1 flex items-center gap-1 text-xs sm:text-sm md:text-md lg:text-lg"><img src="BookmarkSimple.svg" class="{{$bookmarked ? 'invert' : 'invert-0'}}"><p class="hidden md:block">Save</p></button>
+                    <button class="{{$liked ? 'bg-white text-black' : 'border-[#2c2c2c] border-2 text-white'}} rounded p-2 flex items-center gap-1  text-xs sm:text-sm md:text-md lg:text-lg"><img src="ThumbsUp.svg" class="{{$liked ? 'invert' : 'invert-0'}}"><p class="hidden md:block">{{$likes}}</p></button> --}}
+
+                    <button class="border-[#2c2c2c] border-2 text-white rounded px-2 py-1 flex items-center gap-1 text-xs sm:text-sm md:text-md lg:text-lg"><img src="BookmarkSimple.svg" class="invert-0"><p class="hidden md:block">Save</p></button>
+                    <button class="bg-white text-black rounded p-2 flex items-center gap-1  text-xs sm:text-sm md:text-md lg:text-lg"><img src="ThumbsUp.svg" class="invert"><p class="hidden md:block">256</p></button>
+                </div>
+            </div>
+        </x-details>
     </x-title-card>
     <div class="text-white mt-7">
         <h1 class="font-bold text-lg md:text-xl mb-3">About this syllabus</h1>
