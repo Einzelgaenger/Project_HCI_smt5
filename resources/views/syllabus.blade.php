5f1abb39498c0075d47dbb64e02af11226c949b5
Alvin-Winst <alvinrw04@gmail.com>
2024-12-05 23:01:46 +0700

    Update: View Syllabus Course

--- resources/views/syllabus.blade.php
+++ resources/views/syllabus.blade.php
@@ -3,41 +3,68 @@
 @section("title", "Learn")
 
 @section('content')
-<div class="p-6">
-        <x-title-card>
-            <article class="w-4/5">
-                {{-- <div class="flex items-center gap-x-4 text-xs">
-                    <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                    <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-                </div> --}}
-                <div class="my-7 ml-3 mr-6 w-full">
-                    <h3 class="text-2xl w-full lg:w-3/4 font-black text-white sm:text-3xl overflow-hidden line-clamp-2">
-                    Boost your conversion rate Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-                    </h3>
-                    <p class="mt-3 text-xs/5 w-full lg:w-11/12 text-white sm:text-sm/6 xl:text-base h-[6.5rem] sm:h-24 overflow-hidden line-clamp-5 sm:line-clamp-4">Brief description of the syllabus, what to the user expect to learn, what they will do, and so on. Make it a bit more longer to make sure  a long paragraph would fit perfectly to the design. Please don’t read these, it’s for eliminating white spaces. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem illo magni animi nisi accusamus aut quam expedita ut dolorem, itaque, corporis rerum dolores tempore quasi excepturi maiores minus. Nihil veritatis sed provident a, voluptatem, corrupti consequatur minus tempora dicta accusantium ullam cupiditate eligendi impedit reprehenderit asperiores laboriosam non magni ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, nobis id amet adipisci hic voluptates voluptate mollitia reiciendis fugiat, voluptas aspernatur eligendi quam. Non, modi nihil molestiae iure temporibus error perspiciatis id numquam? Corrupti ad veritatis nisi voluptates voluptatem harum, expedita, vitae quam, ipsam delectus architecto explicabo. Perferendis, eum. Quia!</p>
-                </div>
-            </article>
-            @include('components.details', [
-                'difficulty' => 'Beginner Friendly',
-                'duration' => 6,
-                'lessons' => 12,
-                'bookmarked' => false,
-                'liked' => true,
-                'likes' => 256
-            ])
-        </x-title-card>
-        <div class="text-white mt-7">
-            <h1 class="font-bold text-lg md:text-xl mb-3">About this syllabus</h1>
-            <ul class="list-disc ml-6 text-sm md:text-base">
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, a!</li>
-                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quam dolorum fuga amet? Dolorem, perferendis?</li>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, reiciendis! Molestiae, eaque?</li>
-            </ul>
-        </div>
-            <!-- More posts... -->
-        </div>
+<div class="py-6 px-10">
+    <x-title-card>
+        <article class="w-4/5">
+            {{-- <div class="flex items-center gap-x-4 text-xs">
+                <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+                <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+            </div> --}}
+            <div class="my-7 ml-3 mr-6 w-full">
+                <h3 class="text-2xl w-full lg:w-3/4 font-black text-white sm:text-3xl overflow-hidden line-clamp-2">
+                Boost your conversion rate Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                </h3>
+                <p class="mt-3 text-xs/5 w-full lg:w-11/12 text-white sm:text-sm/6 xl:text-base h-[6.5rem] sm:h-24 overflow-hidden line-clamp-5 sm:line-clamp-4">Brief description of the syllabus, what to the user expect to learn, what they will do, and so on. Make it a bit more longer to make sure  a long paragraph would fit perfectly to the design. Please don’t read these, it’s for eliminating white spaces. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem illo magni animi nisi accusamus aut quam expedita ut dolorem, itaque, corporis rerum dolores tempore quasi excepturi maiores minus. Nihil veritatis sed provident a, voluptatem, corrupti consequatur minus tempora dicta accusantium ullam cupiditate eligendi impedit reprehenderit asperiores laboriosam non magni ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, nobis id amet adipisci hic voluptates voluptate mollitia reiciendis fugiat, voluptas aspernatur eligendi quam. Non, modi nihil molestiae iure temporibus error perspiciatis id numquam? Corrupti ad veritatis nisi voluptates voluptatem harum, expedita, vitae quam, ipsam delectus architecto explicabo. Perferendis, eum. Quia!</p>
+            </div>
+        </article>
+        @include('components.details', [
+            'difficulty' => 'Beginner Friendly',
+            'duration' => 6,
+            'lessons' => 12,
+            'bookmarked' => false,
+            'liked' => true,
+            'likes' => 256
+        ])
+    </x-title-card>
+    <div class="text-white mt-7">
+        <h1 class="font-bold text-lg md:text-xl mb-3">About this syllabus</h1>
+        <ul class="list-disc ml-6 text-sm md:text-base">
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, a!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quam dolorum fuga amet? Dolorem, perferendis?</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, reiciendis! Molestiae, eaque?</li>
+        </ul>
+    </div>
+    <div class="text-white mt-12">
+        <h1 class="font-bold text-lg md:text-xl mb-8">Courses</h1>
+        @for ($i = 0; $i < 5; $i++)
+            <div class="flex {{$i % 2 == 0 ? 'justify-start' : 'justify-end'}} mb-12">
+                <form action="#save" method="POST" class="w-[50%] p-1 rounded-[8px] border border-white group/titles">
+                    <div class="bg-[#2F6B4D] h-22 px-8 py-6 rounded-[4px] text-center group-hover/titles:bg-[#CD7C42] transition-colors duration-200">
+                        <h3 class="text-sm sm:text-base lg:text-xl font-semibold w-full truncate">({{$i+1}})&emsp;Course Title {{$i+1}} Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quisquam dicta maiores assumenda dolor quod ipsam iure officia, modi ab doloribus at tempora aliquid reiciendis fugiat repellendus deserunt quia eligendi?</h3>
+                    </div>
+                    <div class="w-full flex-col my-4 hidden group-hover/titles:flex">
+                        @for ($j = 0; $j < 5; $j++)
+                            <div class="w-full flex gap-10 px-4 sm:px-8 md:px-12 lg:px-24 justify-between my-[0.35rem]">
+                                <label for="c{{$i}}m{{$j}}" class="w-full flex items-center gap-10 cursor-pointer">
+                                    <div class="flex w-full gap-2 sm:gap-5 lg:gap-8 2xl:gap-16 group/details">
+                                        <input type="checkbox" name="course-{{$i}} module-{{$j}}" id="c{{$i}}m{{$j}}" class="hidden peer">
+                                        <div class="truncate text-lg transition-colors duration-200 peer-checked:text-[#666] group-hover/details:text-[#a9a9a9]">
+                                            {{$j+1}}.&ensp;Module {{$j+1}} Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt mollitia animi repudiandae ipsum suscipit! Asperiores, nisi beatae. Delectus iste eligendi placeat nisi quasi explicabo quibusdam tenetur vitae ad harum? Optio.
+                                        </div>
+                                        <span class="w-52 md:w-40 xl:w-24 2xl:w-20 h-6 border-2 border-white group-hover/details:border-[#a9a9a9] flex items-center justify-center rounded-sm transition-colors duration-200 peer-checked:hidden relative"></span>
+                                        <span class="w-52 md:w-40 xl:w-24 2xl:w-20 h-6 border-2 border-[#666] hidden items-center justify-center rounded-sm pb-[0.1rem] transition-colors duration-200 peer-checked:flex relative">
+                                            <span class="absolute w-1.5 h-3 border-[#666] border-r-2 border-b-2 transform rotate-45"></span>
+                                        </span>
+                                    </div>
+                                </label>
+                            </div>
+                        @endfor
+                        <button type="submit" class="mt-5 mb-1 w-1/5 h-9 pb-[0.15rem] self-center rounded-[8px] border-2 border-white hover:border-[#a9a9a9] hover:text-[#a9a9a9] font-semibold">Save</button>
+                    </div>
+                </form>
+            </div>
+        @endfor
     </div>
 </div>
-    </div>
 @endsection
