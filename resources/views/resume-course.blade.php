2e441ef0d1059c1f05572dc10bdc96e650471472
Alvin-Winst <alvinrw04@gmail.com>
2024-12-08 16:45:14 +0700

    Added logout in navbar

---
+++ resources/views/resume-course.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.app")
+
+@section("title", "Path in Progress")
+
+@section('content')
+
+    <div>
+        <x-title-card>
+            <div class="text-white w-full sm:w-8/12 my-3 ml-3">
+                <h1 class="text-xl md:text-2xl/9 lg:text-3xl/10 font-bold mb-4">My Learning</h1>
+            </div>
+        </x-title-card>
+    </div>
+
+@endsection
