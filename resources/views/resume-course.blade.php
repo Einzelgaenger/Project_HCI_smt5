77e861f0c4ca12ff0b4b8be9ac460633806bbde0
Alvin-Winst <alvinrw04@gmail.com>
2024-12-08 21:54:17 +0700

    Updated module details in resume-course

--- resources/views/resume-course.blade.php
+++ resources/views/resume-course.blade.php
@@ -34,34 +34,63 @@
                 </x-details>
             </div>
         </x-title-card>
-        <div class="my-12">
-            <h1 class="text-white text-xl font-semibold mb-10">Module</h1>
-            <form action="#" method="POST" class="w-full">
-                <div class="rounded-[12px] border border-white py-12 px-16 flex flex-col gap-6">
+        <div class="mt-12 mb-3">
+            <form action="/" method="POST" class="w-full">
+                <div class="w-full my-5 flex mb-7 items-center gap-10">
+                    <h1 class="text-white text-xl font-semibold">Module</h1>
+                    <button type="submit" class="w-[13%] font-medium py-2 text-white border-white border rounded-3xl text-sm hover:bg-white hover:text-black">Save Progress</button>
+                </div>
+                <div class="rounded-[12px] border border-white py-12 px-16 flex flex-col gap-12">
                 @for ($i = 0; $i < 6; $i++)
-                    <div class="text-white flex justify-between">
-                        <label for="module-{{$i}}" class="w-full flex items-center gap-10 cursor-pointer my-1">
-                            <div class="flex w-full gap-8 group/details items-center">
-                                <input type="checkbox" name="module-{{$i}}" id="module-{{$i}}" value="module-{{$i}}" class="module hidden peer">
-                                <span class="w-5 h-5 border-2 border-white group-hover/details:border-[#a9a9a9] flex items-center justify-center rounded-sm transition-colors duration-200 peer-checked:hidden"></span>
-                                <span class="w-5 h-5 border-2 bg-white hidden items-center justify-center rounded-sm pb-[0.1rem] transition-colors duration-200 peer-checked:flex">
-                                    <span class="absolute w-1.5 h-3 border-black border-r-2 border-b-2 transform rotate-45"></span>
-                                </span>
-                                <div class="truncate text-lg w-[54rem] transition-colors duration-200 group-hover/details:text-[#a9a9a9] font-semibold">
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus corrupti nostrum voluptates architecto placeat nemo animi itaque possimus nobis, harum, repellendus qui tenetur doloribus esse fuga eaque aliquid asperiores! Perspiciatis!
-                                </div>
-                            </div>
-                        </label>
-                        <button class="rotate-180"><img src="CaretUp.svg" alt=""></button>
-                    </div>
                     <div>
-
+                        <div class="text-white flex justify-between">
+                            <label for="module-{{$i}}" class="w-full flex items-center gap-10 cursor-pointer my-1">
+                                <div class="flex w-full gap-10 group/details items-center">
+                                    <input type="checkbox" name="module-{{$i}}" id="module-{{$i}}" value="module-{{$i}}" class="module hidden peer">
+                                    <span class="w-6 h-6 border-2 border-white group-hover/details:border-[#a9a9a9] flex items-center justify-center rounded-sm transition-colors duration-200 peer-checked:hidden"></span>
+                                    <span class="w-6 h-6 border-2 bg-white hidden items-center justify-center rounded-sm pb-[0.1rem] transition-colors duration-200 peer-checked:flex">
+                                        <span class="absolute w-1.5 h-3 border-black border-r-2 border-b-2 transform rotate-45"></span>
+                                    </span>
+                                    <div class="truncate text-lg w-[54rem] transition-colors duration-200 group-hover/details:text-[#a9a9a9] font-semibold">
+                                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus corrupti nostrum voluptates architecto placeat nemo animi itaque possimus nobis, harum, repellendus qui tenetur doloribus esse fuga eaque aliquid asperiores! Perspiciatis!
+                                    </div>
+                                </div>
+                            </label>
+                            <button type="button" class="expand rotate-180" onclick="expandCollapse(event)"><img src="CaretUp.svg" alt=""></button>
+                        </div>
+                        <hr class="separator ml-16 my-2 hidden">
+                        <div class="expand-details text-white ml-[4.1rem] my-3 hidden">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quidem sunt nihil aliquid, facilis ipsum dignissimos iste consequuntur commodi! Cumque minus fugiat quisquam nesciunt illo quas officia voluptatibus vel ea!</p>
+                            <p class="my-2">Link:&ensp;<a href="https://youtube.com" class="text-[#84C8FF] font-medium">https://youtube.com</a></p>
+                        </div>
                     </div>
                 @endfor
                 </div>
-                <button type="submit" class="w-1/2 font-medium py-1 text-white border-white border rounded-3xl px-5 hover:bg-white hover:text-black">Save</button>
             </form>
         </div>
     </div>
+    <script>
+        expandBtn = document.getElementsByClassName('expand')
+        separator = document.getElementsByClassName('separator')
+        expandDetails = document.getElementsByClassName('expand-details')
 
+        for (let index = 0; index < expandBtn.length; index++) {
+            expandBtn[index].addEventListener('click', function (){
+                if (expandBtn[index].classList.contains('rotate-180')) {
+                    expandBtn[index].classList.remove('rotate-180')
+                    separator[index].classList.remove('hidden')
+                    expandDetails[index].classList.remove('hidden')
+                }
+                else{
+                    expandBtn[index].classList.add('rotate-180')
+                    if (!separator[index].classList.contains('hidden')) {
+                        separator[index].classList.add('hidden')
+                    }
+                    if (!expandDetails[index].classList.contains('hidden')) {
+                        expandDetails[index].classList.add('hidden')
+                    }
+                }
+            })
+        }
+    </script>
 @endsection
